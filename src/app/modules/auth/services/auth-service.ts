95b26e7977080e86e361192679bdd7ab5133705c
shikhagupta23 <shikhagupta@resiliencesoft.com>
2026-01-14 13:21:33 +0530

    FixedRefreshIssue

--- src/app/modules/auth/services/auth-service.ts
+++ src/app/modules/auth/services/auth-service.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
   providedIn: 'root',
 })
 export class AuthService {
-  private userSubject = new BehaviorSubject<any>(null);
+  private userSubject = new BehaviorSubject<any>(this.getUser());
   user$ = this.userSubject.asObservable();
   private api = inject(ApiService);
   private router = inject(Router);
